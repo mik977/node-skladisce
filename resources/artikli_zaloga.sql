e7e1fc43e09de1fa20295929ef84a7caba7db094
Miha Kralj <miha@link-go.si>
2013-06-20 09:08:05 +0200

    Končna verzija za isql

--- resources/artikli_zaloga.sql
+++ resources/artikli_zaloga.sql
@@ -1,1 +1,3 @@
-select D.OPIS as Del, S.OPIS as Opis, Z.OPIS as Znamka, M.OPIS as Model, S.CAS_VNOSA as Vnos, S.CAS_IZNOSA as Iznos from SKLADISCE S, AVTO_DEL D, AVTO_MODEL M, AVTO_ZNAMKA Z where S.ID_AVTO=M.AUTONUM and M.ID_ZNAMKA=Z.AUTONUM and S.ID_AVTO_DEL=D.AUTONUM and S.CAS_IZNOSA is NULL order by Z.OPIS, M.OPIS
+set heading;
+set list;
+select D.OPIS as Del, S.OPIS as Opis, Z.OPIS as Znamka, M.OPIS as Model, S.CAS_VNOSA as Vnos, S.CAS_IZNOSA as Iznos from SKLADISCE S, AVTO_DEL D, AVTO_MODEL M, AVTO_ZNAMKA Z where S.ID_AVTO=M.AUTONUM and M.ID_ZNAMKA=Z.AUTONUM and S.ID_AVTO_DEL=D.AUTONUM and S.CAS_IZNOSA is NULL order by Z.OPIS, M.OPIS;
